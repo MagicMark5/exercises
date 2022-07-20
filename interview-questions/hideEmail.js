71237e216caf1966bf57e543f1cb62422d156e60
Adam Marsala <adammarsala@hotmail.com>
2022-07-20 01:29:50 -0400

    organize repo

--- interview-questions/hideEmail.js
+++ interview-questions/hideEmail.js
@@ -1,3 +1,11 @@
+/*
+Given a string that has a valid email address, 
+write a function to hide the first part of the email (before the @ sign), 
+minus the first and last character. 
+For extra credit, add a flag to hide the second part after the @ sign 
+to your function excluding the first character and the domain extension.
+*/
+
 function hideEmail(email = 'example+test@example.co.uk', hideFull) {
   // split a string into 2 parts before & after a given character in that string
   function splitStringAt(string, char) {
